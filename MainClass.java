da3584a6c1931e9f2c28106ce2b514648894c325
Piyush <gaurpiyush001@gmail.com>
2022-01-29 02:10:34 +0530

    final(29-1-22)

--- MainClass.java
+++ MainClass.java
@@ -2,6 +2,7 @@ package basics;
 
 public class MainClass {
 	
+	//this below static keyword suggest that if we want to access the main method of MainClass then it is not necessary to create an Object of MainClass
 	public static void main(String[] args) {
 //		System.out.println("Hello World");
 		Person p1 = new Person();
@@ -12,11 +13,16 @@ public class MainClass {
 		p2.age = 27;
 		p2.name = "Kuch Bhiiii!!";
 		
+		/*
 		System.out.println(p1.name + " " + p1.age);
 		System.out.println(p2.name + " " + p2.age);
+		*/
+		
 		p1.eat();
 		p2.walk();
 		p1.walk(23);
+		
+		System.out.println("No. of times contructor called" + Person.count);
 	}
 
 }
@@ -27,6 +33,15 @@ class Person{
 	////////////////////////////////////PROPERTIES///////////////////////////////////////
 	String name;
 	int age;
+	
+	
+	//static keyword-: gives class a property
+	static int count;//this is now a property of class means to access this property we don't need to create an object
+	
+	public Person() {
+		count++;
+		System.out.println("creating/constructing an object");
+	}
 	
 	////////////////////////////////BEHAVIOR(METHODS)////////////////////////////////
 	void walk() {
